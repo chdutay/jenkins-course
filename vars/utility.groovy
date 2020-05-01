f4e489342c035f5e3fff471f3d0f110410f2ae59
chdutay <>
2020-05-01 16:43:51 +0200

    Update utility.groovy

--- vars/utility.groovy
+++ vars/utility.groovy
@@ -3,3 +3,13 @@
 def getBuildUser() {
     return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
+
+// valid if string as date format dd-mm-yyyy
+def isValidStringDate(String date) {
+    return date ==~ /((0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-[12]\d{3})/
+}
+
+// Return string with current date with format dd-mm-yyyy
+def getStringDate() {
+    return "date +%d-%m-%Y".execute().text
+}
